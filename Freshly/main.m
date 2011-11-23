16ec43251cb3c8873f9065e70edb279c38f498a3
Enrico "cHoco" Ghirardi <just@adream.me>
2011-11-23 10:42:54 +0100

    Initial Commit

---
+++ Freshly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Freshly
+//
+//  Created by Enrico Ghirardi on 23/11/11.
+//  Copyright (c) 2011 !cHoco Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JaDFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JaDFAppDelegate class]));
+    }
+}
